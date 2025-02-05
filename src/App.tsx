e7cd1b3d870d5dc61c8b904483c058abc6ec6b61
francelino <fmarcilio1989@gmail.com>
2025-02-05 20:08:06 -0300

    feat: adding shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Button } from "./components/ui/button";
 
 export function App() {
 
   return (
-    <>
-
-    </>
+    <div className="flex justify-center items-center h-screen">
+      <Button>enviar</Button>
+    </div>
   )
 }
