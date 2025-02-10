df91e49f084f21165a8404c039f4669184045a97
francelino <fmarcilio1989@gmail.com>
2025-02-09 22:03:47 -0300

    feat: page sing up

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
     email: z.string().email(),
@@ -39,6 +40,14 @@ export function SignIn() {
             <Helmet title="Login" />
 
             <div className="p-8">
+
+                <Button variant="outline" asChild className="absolute top-8 right-8">
+                    <Link to="/sign-up">
+                        Novo Estabelecimento
+                    </Link>
+                </Button>
+
+
                 <div className="flex w-[350px] flex-col justify-center gap-6">
                     <div className="flex flex-col gap-2 text-center">
                         <h1 className="text-2xl font-semibold tracking-tight">
